4a6e70fe4a86bf37eecbb70fac47a639881c04bd
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-09 07:30:04 +0300

    Update 09. Square of Stars.js

--- 01. Syntax, Functions and Statements/Lab/09. Square of Stars.js
+++ 01. Syntax, Functions and Statements/Lab/09. Square of Stars.js
@@ -1,6 +1,17 @@
-function printSquare(n = 5) {
-    const row = "* ".repeat(n).trim()
-	for (let i = 0; i < n; i++) {
-		console.log(row)
-	}
+// function printSquare(n = 5) {
+//     const row = "* ".repeat(n).trim()
+// 	for (let i = 0; i < n; i++) {
+// 		console.log(row)
+// 	}
+// }
+
+
+function solve(size) {
+    for (let row = 1; row <= size; row++) {
+        let line = '';
+        for (let col = 1; col <= size; col++) {
+            line += '* ';
+        }
+        console.log(line);
+    }
 }
