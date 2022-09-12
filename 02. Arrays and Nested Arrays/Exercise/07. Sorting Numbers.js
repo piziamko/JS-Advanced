8f243aa3c7d6d01849164dce6d7c1bb65e354ce5
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-12 22:42:32 +0300

    Update 07. Sorting Numbers.js

--- 02. Arrays and Nested Arrays/Exercise/07. Sorting Numbers.js
+++ 02. Arrays and Nested Arrays/Exercise/07. Sorting Numbers.js
@@ -1,17 +1,29 @@
+function solve(array) {
+    array.sort((a, b) => a - b);
+    let middle = Math.ceil(array.length / 2);
+    let biggerHalf = array.splice(middle).reverse();
+    let smallerHalf = array;
+    let result = smallerHalf.reduce((acc, a, i) => acc.concat([a, biggerHalf[i]]), []);
+    return result;
+}
+
+// console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
+
+
 //7.	Sorting Numbers
 
-function solve1(arr) {
-    let result = [];
+// function solve1(arr) {
+//     let result = [];
 
-    arr.sort((a,b) => a- b);
+//     arr.sort((a,b) => a- b);
 
-    while(arr.length){
-        result.push(arr.shift());
-        result.push(arr.pop());
-    }
+//     while(arr.length){
+//         result.push(arr.shift());
+//         result.push(arr.pop());
+//     }
 
-    return result.filter(num => num != undefined);
-}
+//     return result.filter(num => num != undefined);
+// }
 
 //console.log(solve1(
 //    [1, 65, 3, 52, 48, 63, 31, -3, 18, 56]
