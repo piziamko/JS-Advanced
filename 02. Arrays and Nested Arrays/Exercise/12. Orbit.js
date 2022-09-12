d630d45d9511ce5ef78cfd01b03bbb63260d936a
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-12 23:00:37 +0300

    Update 12. Orbit.js

--- 02. Arrays and Nested Arrays/Exercise/12. Orbit.js
+++ 02. Arrays and Nested Arrays/Exercise/12. Orbit.js
@@ -6,3 +6,14 @@ function foo(arr) {
         .map(x => x.join(" "))
         .join("\n")
 }
+
+// Variant 2
+
+function foo(arr) {
+    let matrix = new Array(arr[0]).fill(new Array(arr[1]).fill(''))
+
+    return matrix
+        .map((x, i) => x.map((y, j) => Math.max(Math.abs(j - arr[3]), Math.abs(i - arr[2])) + 1))
+        .map(x => x.join(" "))
+        .join("\n")
+}
