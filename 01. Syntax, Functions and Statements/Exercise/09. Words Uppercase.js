2fe19a7d18d514ed0e9168f4c5400e1ded05c940
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-12 21:21:54 +0300

    Update 09. Words Uppercase.js

--- 01. Syntax, Functions and Statements/Exercise/09. Words Uppercase.js
+++ 01. Syntax, Functions and Statements/Exercise/09. Words Uppercase.js
@@ -9,3 +9,15 @@ function solve(text) {
 }
 
 //solve('Hi, how are you?');
+
+//Variante 2
+
+// function solve(text){
+//     let regex = /(\w+)/g
+//     let matches = text.match(regex)
+//     matches = matches.map(word => word.toUpperCase())
+//     console.log(matches.join(', '))
+// }
+
+// solve('Hi, how are you?');
+// solve('hello');
