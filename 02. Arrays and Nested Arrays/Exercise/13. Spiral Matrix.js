513186aa63b6c7815b9c74182fd346883865b3d4
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-12 23:07:34 +0300

    Update 13. Spiral Matrix.js

--- 02. Arrays and Nested Arrays/Exercise/13. Spiral Matrix.js
+++ 02. Arrays and Nested Arrays/Exercise/13. Spiral Matrix.js
@@ -1,3 +1,35 @@
+function solve(matrixRow, matrixCol) {
+    let matrix = new Array(matrixRow).fill(undefined).map(() => new Array(matrixCol).fill(-1));
+    let currentNumber = 0;
+    let maxNumber = matrixRow * matrixCol;
+    let [row, col] = [0, -1];
+    const directions = [
+        (r, c) => [r, c + 1],
+        (r, c) => [r + 1, c],
+        (r, c) => [r, c - 1],
+        (r, c) => [r - 1, c],
+    ];
+    let direction = directions.shift();
+
+    while (currentNumber < maxNumber) {
+        let [nextRow, nextCol] = direction(row, col);
+
+        if (!(matrix[nextRow] && matrix[nextRow][nextCol]) || matrix[nextRow][nextCol] !== -1) {
+            directions.push(direction);
+            direction = directions.shift();
+            continue;
+        }
+
+        [row, col] = [nextRow, nextCol];
+        matrix[row][col] = ++currentNumber;
+
+    }
+
+    matrix.forEach(line => console.log(line.join(' ')))
+}
+
+// Variant 2
+
 function foo(w, h) {
     let matrix = new Array(h).fill(new Array(w).fill([]))
     matrix = matrix.map(x => x.map(y => ""))
