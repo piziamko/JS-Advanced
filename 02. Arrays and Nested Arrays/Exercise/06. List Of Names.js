506f6e01262d6b92a1362ea31df3cdd3f69428a7
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-12 22:37:48 +0300

    Update 06. List Of Names.js

--- 02. Arrays and Nested Arrays/Exercise/06. List Of Names.js
+++ 02. Arrays and Nested Arrays/Exercise/06. List Of Names.js
@@ -1,14 +1,20 @@
+function solve(names) {
+    names.sort((a, b) => a.localeCompare(b));
+    names.forEach((name, index) => console.log(`${index + 1}.${name}`));
+}
+
+
 //6.	List of Names
 
-function sortNames(names) {
-    return names
-        .slice(0)
-        .sort((a, b) => a.localeCompare(b))
-        .map((name, index) => {
-            return `${index + 1}.${name}`;
-        })
-        .join('\n');
-}
+// function sortNames(names) {
+//     return names
+//         .slice(0)
+//         .sort((a, b) => a.localeCompare(b))
+//         .map((name, index) => {
+//             return `${index + 1}.${name}`;
+//         })
+//         .join('\n');
+// }
 
 // console.log(sortNames(
 //     ["John", "Bob", "Christina", "Ema"]
