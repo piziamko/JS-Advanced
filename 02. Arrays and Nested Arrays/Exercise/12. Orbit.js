bc8aa41edfb495b1044ef515902b8c3e49178242
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-12 23:03:06 +0300

    Update 12. Orbit.js

--- 02. Arrays and Nested Arrays/Exercise/12. Orbit.js
+++ 02. Arrays and Nested Arrays/Exercise/12. Orbit.js
@@ -1,10 +1,14 @@
-function foo(arr) {
-    let matrix = new Array(arr[0]).fill(new Array(arr[1]).fill(''))
+function solveSmarterWay(array) {
+    let [matrixRows, matrixCols, startingRow, startingCol] = array;
+    let matrix = new Array(matrixRows).fill(true).map(() => new Array(matrixCols).fill(true));
 
-    return matrix
-        .map((x, i) => x.map((y, j) => Math.max(Math.abs(j - arr[3]), Math.abs(i - arr[2])) + 1))
-        .map(x => x.join(" "))
-        .join("\n")
+    for(let row = 0; row < matrix.length; row++){
+        for(let col = 0; col < matrix[row].length; col ++){
+        matrix[row][col] = Math.max(Math.abs(row - startingRow), Math.abs(col - startingCol)) + 1
+        }
+    }
+
+    matrix.forEach(line => console.log(line.join(' ')))
 }
 
 // Variant 2
