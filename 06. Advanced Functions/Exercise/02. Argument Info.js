0c035043772c951d98cb278ee750e2de3618e9b2
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 10:52:42 +0300

    Update 02. Argument Info.js

--- 06. Advanced Functions/Exercise/02. Argument Info.js
+++ 06. Advanced Functions/Exercise/02. Argument Info.js
@@ -16,3 +16,18 @@ function findTypeOfInput() {
         .forEach((key) => console.log(`${key} = ${data[key]}`));
 
 }
+
+//  Variant 2 
+
+function solve() {
+    const result = {};
+    for (const arg of arguments) {
+        const type = typeof arg;
+        console.log(`${type}: ${arg}`);
+        result[type] = (result[type] || 0)+1;
+    }
+    Object.entries(result)
+        .sort((a,b) => b[1] - a[1])
+        .forEach(
+            kvp => console.log(`${kvp[0]} = ${kvp[1]}`));
+}
