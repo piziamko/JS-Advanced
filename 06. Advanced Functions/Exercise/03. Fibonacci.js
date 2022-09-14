0298a80b55e197799e61ec1ad8f9de7ea420dbf1
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 10:56:19 +0300

    Update 03. Fibonacci.js

--- 06. Advanced Functions/Exercise/03. Fibonacci.js
+++ 06. Advanced Functions/Exercise/03. Fibonacci.js
@@ -1,13 +1,13 @@
-function getFibonator() {
-    let [prev, curr] = [1, 0];
+// function getFibonator() {
+//     let [prev, curr] = [1, 0];
 
-    return () => {
-        let next = prev + curr;
-        prev = curr;
-        curr = next;
-        return curr;
-    }
-}
+//     return () => {
+//         let next = prev + curr;
+//         prev = curr;
+//         curr = next;
+//         return curr;
+//     }
+// }
 
 //let fib = getFibonator();
 //console.log(fib());
@@ -17,3 +17,18 @@ function getFibonator() {
 //console.log(fib());
 //console.log(fib());
 //console.log(fib());
+
+// Variant 2
+
+function solve() {
+    const lastNumbers = [0, 0];
+
+    function fib() {
+        const result = lastNumbers.reduce((acc, num) => acc + num, 0) || 1;
+        lastNumbers.shift()
+        lastNumbers.push(result)
+        return result
+    }
+
+    return fib;
+}
