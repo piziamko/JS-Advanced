63c28982886d2c708728d3eaec93fbaef63c70d2
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 09:18:00 +0300

    Update script.js

--- 05. DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ 05. DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -21,3 +21,25 @@ function create(arr) {
 
     elements.forEach(x => output.appendChild(x))
 }
+
+// Variant 2
+
+function create(strings) {
+
+    function display(e) {
+        e.target.firstChild.style.display = 'inline-block';
+    }
+
+    for (const string of strings) {
+        const currentPElement = document.createElement('p');
+        currentPElement.textContent = string;
+        currentPElement.style.display = 'none';
+
+        const currentDivElement = document.createElement('div');
+        currentDivElement.appendChild(currentPElement);
+        currentDivElement.addEventListener('click', display);
+
+        document.getElementById('content').appendChild(currentDivElement);
+    }
+
+}
