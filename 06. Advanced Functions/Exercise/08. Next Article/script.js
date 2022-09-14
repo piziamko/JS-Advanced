0750d02b7a45ad12700fb71c75f5420071fe41bb
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 12:11:49 +0300

    Update script.js

--- 06. Advanced Functions/Exercise/08. Next Article/script.js
+++ 06. Advanced Functions/Exercise/08. Next Article/script.js
@@ -12,3 +12,17 @@ function getArticleGenerator(articles) {
         return showNext
     }
 }
+
+// Variant 2 
+
+function getArticleGenerator(articles) {
+    const ID_CONTENT_DIV = 'content';
+
+    return () => {
+        if (!articles.length) return;
+
+        const articleElement = document.createElement('article');
+        articleElement.textContent = articles.shift();
+        document.getElementById(ID_CONTENT_DIV).appendChild(articleElement);
+    };
+}
