4376142beaee985ba550972417750314847ccf1c
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 13:23:16 +0300

    Update 06. Company.js

--- 08. Classes/Exercise/06. Company.js
+++ 08. Classes/Exercise/06. Company.js
@@ -1,64 +1,49 @@
-// WORKING CODE
-
 class Company {
-    constructor() {
-        this.departments = [];
-    }
- 
-    addEmployee(username, salary, position, department) {
-        if ([...arguments].some(a => a === null || a === undefined || a === '') || salary < 0) {
-            throw new Error('Invalid input!');
-        } else {
-            const newEmployee = {
-                username: username,
-                salary: salary,
-                position: position,
-                department: department,
-            };
-            if (this.departments.filter(function (e) {return e.name === department;}).length > 0) {
-                for (let existingDepartment of this.departments) {
-                    if (existingDepartment.name === department) {
-                        existingDepartment.users.push(newEmployee);
-                        existingDepartment.totalSalary += salary;
-                    }
-                }
-            } else {
-                let newDepartment = {
-                    name: department,
-                    users: [newEmployee],
-                    totalSalary: salary,
-                    averageSalary() {
-                        return this.totalSalary / this.users.length;
-                    },
-                };
-                this.departments.push(newDepartment);
-            }
-            return `New employee is hired. Name: ${username}. Position: ${position}`;
+    departments = {};
+    departmentClass = class Department {
+        employees = [];
+
+        constructor(name) {
+            this.name = name;
         }
+
+        addEmployee(name, salary, position) {
+            this.employees.push({name, salary, position});
+        }
+
+        getDepartmentInfo() {
+            return `Average salary: ${this.getAverageSalary().toFixed(2)}\n` + this.employees
+                .sort((a, b) => (b.salary - a.salary) || (a.name.localeCompare(b.name)))
+                .map(worker => `${worker.name} ${worker.salary} ${worker.position}`)
+                .join('\n');
+        }
+
+        getAverageSalary() {
+            return this.employees.reduce((acc, employee) => acc + employee.salary, 0) / this.employees.length;
+        }
+    };
+
+
+    addEmployee(name, salary, position, department) {
+        if (salary < 0
+            || arguments.length < 4
+            || Array.from(arguments).some(x => x === '' || x === undefined || x === null)) {
+            throw Error('Invalid input!');
+        }
+
+        if (!this.departments[department]) {
+            this.departments[department] = new this.departmentClass(department);
+        }
+
+        this.departments[department].addEmployee(name, salary, position);
+        return `New employee is hired. Name: ${name}. Position: ${position}`;
     }
- 
+
     bestDepartment() {
-        let bestDepartment = this.departments.sort((a, b) => a.averageSalary - b.averageSalary)[0];
-        bestDepartment.users = bestDepartment.users.sort(function (a, b) {
-            if (a.username === b.username) {
-                return b.username - a.username;
-            }
-            return a.salary < b.salary ? 1 : -1;
-        });
-        let result = `Best Department is: ${bestDepartment.name}\nAverage salary: ${bestDepartment.averageSalary().toFixed(2)}`;
-        for (let user of bestDepartment.users) {
-            result += `\n${user.username} ${user.salary} ${user.position}`;
-        }
+        const bestDepartment = Object.values(this.departments)
+            .sort((a, b) => b.getAverageSalary() - a.getAverageSalary())[0];
+        let result = `Best Department is: ${bestDepartment.name}\n`;
+        result += bestDepartment.getDepartmentInfo();
         return result;
     }
 }
-
-let c = new Company();
-c.addEmployee("Stanimir", 2000, "engineer", "Construction");
-c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
-c.addEmployee("Slavi", 500, "dyer", "Construction");
-c.addEmployee("Stan", 2000, "architect", "Construction");
-c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
-c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
-c.addEmployee("Gosho", 1350, "HR", "Human resources");
-console.log(c.bestDepartment());
