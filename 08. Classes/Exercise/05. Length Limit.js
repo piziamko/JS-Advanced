10f1713eb4088c840edaad63e225e135f6c9e9d3
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 13:19:12 +0300

    Update 05. Length Limit.js

--- 08. Classes/Exercise/05. Length Limit.js
+++ 08. Classes/Exercise/05. Length Limit.js
@@ -28,11 +28,11 @@ class Stringer {
     }
 }
 
-let test = new Stringer("Test", 5);
-console.log(test.toString()); // Test
-test.decrease(3);
-console.log(test.toString()); // Te...
-test.decrease(5);
-console.log(test.toString()); // ...
-test.increase(4);
-console.log(test.toString()); // Test 
+// let test = new Stringer("Test", 5);
+// console.log(test.toString()); // Test
+// test.decrease(3);
+// console.log(test.toString()); // Te...
+// test.decrease(5);
+// console.log(test.toString()); // ...
+// test.increase(4);
+// console.log(test.toString()); // Test 
