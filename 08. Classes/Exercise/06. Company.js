82baab72dd608ad87268617834b0dd493ca9d582
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 13:26:16 +0300

    Update 06. Company.js

--- 08. Classes/Exercise/06. Company.js
+++ 08. Classes/Exercise/06. Company.js
@@ -34,6 +34,46 @@ class Company {
         if (!this.departments[department]) {
             this.departments[department] = new this.departmentClass(department);
         }
+        
+// Variant 2
+        
+        class Company {
+    departments = {};
+
+    addEmployee(name, salary, position, department) {
+        if (salary < 0
+            || arguments.length < 4
+            || Array.from(arguments).some(x => x==='' || x=== undefined || x===null)) {
+            throw Error('Invalid input!');
+        }
+
+        const worker = {name, salary, position};
+
+        if (!this.departments[department]) {
+            this.departments[department] = {averageSalary: 0, workers: []};
+        }
+
+        const averageSalary = this.departments[department].averageSalary;
+        const workersNumber = this.departments[department].workers.length;
+        const totalSalary = averageSalary * workersNumber + salary;
+        const newAverageSalary = totalSalary / (workersNumber + 1);
+        this.departments[department].workers.push(worker);
+        this.departments[department].averageSalary = newAverageSalary;
+        return `New employee is hired. Name: ${name}. Position: ${position}`;
+    }
+
+    bestDepartment() {
+        const [bestDepartmentName, bestDepartmentInfo] = Object.entries(this.departments)
+            .sort((departA, departB) => departB[1].averageSalary - departA[1].averageSalary)[0];
+        let result = `Best Department is: ${bestDepartmentName}\n`;
+        result += `Average salary: ${bestDepartmentInfo.averageSalary.toFixed(2)}\n`;
+        result += bestDepartmentInfo.workers
+            .sort((a, b) => (b.salary - a.salary) || (a.name.localeCompare(b.name)))
+            .map(worker => `${worker.name} ${worker.salary} ${worker.position}`)
+            .join('\n');
+        return result;
+    }
+}
 
         this.departments[department].addEmployee(name, salary, position);
         return `New employee is hired. Name: ${name}. Position: ${position}`;
