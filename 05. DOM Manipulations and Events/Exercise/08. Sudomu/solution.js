bb62f758a60fdec07ac4f05adcc530c45c6d23ea
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 09:37:05 +0300

    Update solution.js

--- 05. DOM Manipulations and Events/Exercise/08. Sudomu/solution.js
+++ 05. DOM Manipulations and Events/Exercise/08. Sudomu/solution.js
@@ -1,70 +1,68 @@
 function solve() {
-    const html = {
-        row: r =>
-            document
-                .querySelector(`#exercise > table > tbody > tr:nth-child(${r})`)
-                .querySelectorAll("td > input"),
-        outputDiv: document.querySelector("#check p"),
-        table: document.querySelector("#exercise > table"),
-        inputs: document.getElementsByTagName("input"),
-    }
+    const DONE_BORDER_STYLE = '2px solid green';
+    const UNDONE_BORDER_STYLE = '2px solid red';
+    const DONE_TEXT_TO_DISPLAY = 'You solve it! Congratulations!';
+    const UNDONE_TEXT_TO_DISPLAY = 'NOP! You are not done yet...';
+    const DONE_TEXT_COLOR = 'green';
+    const UNDONE_TEXT_COLOR = 'red';
 
-    const getDataMatrix = () => {
-        const cell = []
+    const gameField = document.querySelector('tbody');
+    const FieldSize = gameField.children.length;
+    const [buttonCheckElement, buttonResetElement] = document.querySelectorAll('button');
+    const tableElement = document.querySelector('table');
+    const resultOutputElement = document.querySelector('#check p');
 
-        for (let i = 0; i < 3; i++) {
-            const values = Array.from(html.row(i + 1)).map(
-                x => Number(x.value) || 0
-            )
-            cell.push(values)
-        }
+    buttonResetElement.addEventListener('click', clear);
 
-        return cell
-    }
-
-    const isValid = m => {
-
-        const checkRepeatance = arr =>
-            arr.some((x, i) => arr.slice(i + 1).some(y => y === x))
-
-        for (let i = 0; i < m[0].length; i++) {
-            let column = []
-            for (let j = 0; j < m.length; j++) {
-                column.push(m[j][i])
-            }
-
-            if (checkRepeatance(m[i]) || checkRepeatance(column)) return false
-            column = []
-        }
-
-        return true
-    }
-
-    document.addEventListener("click", e => {
-        if (e.target.tagName === "BUTTON") {
-
-            const changeOutput = (b = "", m = "", c = "") => {
-                html.table.style.border = b
-                html.outputDiv.innerHTML = m
-                html.outputDiv.style.color = c
-            }
-
-            if (e.target.innerHTML.includes("Check")) {
-                isValid(getDataMatrix())
-                    ? changeOutput(
-                          "2px solid green",
-                          "You solve it! Congratulations!",
-                          "green"
-                      )
-                    : changeOutput(
-                          "2px solid red",
-                          "NOP! You are not done yet...",
-                          "red"
-                      )
-            } else {
-                changeOutput()
-                Array.from(html.inputs).map(x => (x.value = ""))
+    function clear() {
+        helpers.changeView();
+        for (const row of gameField.children) {
+            for (const col of row.children) {
+                col.firstElementChild.value = '';
             }
         }
-    })
+    }
+
+
+    buttonCheckElement.addEventListener('click', check);
+
+    function check() {
+        const rowsInput = new Array(FieldSize).fill(undefined).map(x => []);
+        const colsInput = new Array(FieldSize).fill(undefined).map(x => []);
+
+        for (let rowIndex = 0; rowIndex < FieldSize; rowIndex++) {
+            for (let colIndex = 0; colIndex < FieldSize; colIndex++) {
+                const cellValue = gameField.children[rowIndex].children[colIndex].firstElementChild.value;
+                rowsInput[rowIndex].push(cellValue);
+                colsInput[colIndex].push(cellValue);
+            }
+        }
+
+        const isDone = helpers.checkIsDone(rowsInput) ? helpers.checkIsDone(colsInput) : false;
+        if (isDone) {
+            helpers.changeView(DONE_BORDER_STYLE, DONE_TEXT_TO_DISPLAY, DONE_TEXT_COLOR);
+        } else {
+            helpers.changeView(UNDONE_BORDER_STYLE, UNDONE_TEXT_TO_DISPLAY, UNDONE_TEXT_COLOR);
+        }
+    }
+
+
+    const helpers = {
+        checkIsDone(checkedDirection) {
+            for (const Numbers of checkedDirection) {
+                if (!(
+                    Numbers.every(x => x >= 1 && x <= checkedDirection.length) &&
+                    new Set(Numbers).size === checkedDirection.length)) {
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        changeView(borderStyle = '', textToDisplay = '', textColor = '') {
+            tableElement.style.border = borderStyle;
+            resultOutputElement.textContent = textToDisplay;
+            resultOutputElement.style.color = textColor;
+        }
+    };
 }
