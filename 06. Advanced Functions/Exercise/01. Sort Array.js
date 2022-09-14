638fed68716cdc55c1e70507076bd7332fb959d8
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 10:49:57 +0300

    Update 01. Sort Array.js

--- 06. Advanced Functions/Exercise/01. Sort Array.js
+++ 06. Advanced Functions/Exercise/01. Sort Array.js
@@ -1,8 +1,14 @@
-function sort(array, argument) {
-    const sort = {
-        asc: (array) => array.sort((a, b) => a - b),
-        desc: (array) => array.sort((a, b) => b - a)
-    };
+// function sort(array, argument) {
+//     const sort = {
+//         asc: (array) => array.sort((a, b) => a - b),
+//         desc: (array) => array.sort((a, b) => b - a)
+//     };
 
-    return sort[argument](array);
+//     return sort[argument](array);
+// }
+
+// Variant 2
+
+function sorting(array, order) {
+    return array.sort((a, b) => order === 'asc' ? a - b : b - a);
 }
