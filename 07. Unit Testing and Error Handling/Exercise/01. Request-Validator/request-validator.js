d965483b584dd4068e040d58ea8d336e05967331
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-14 12:34:46 +0300

    Update request-validator.js

--- 07. Unit Testing and Error Handling/Exercise/01. Request-Validator/request-validator.js
+++ 07. Unit Testing and Error Handling/Exercise/01. Request-Validator/request-validator.js
@@ -1,51 +1,31 @@
-function RequestValidator(input) {
+// function RequestValidator(input) {
 
-    let validMethods = ['GET', 'POST', 'DELETE', 'CONNECT'];
-    let uriRegex = /^[a-zA-Z0-9.]+$|^\*$/g;
-    let validVersions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
-    let message = /^[^<>\\&'"]*$/g;
+//     let validMethods = ['GET', 'POST', 'DELETE', 'CONNECT'];
+//     let uriRegex = /^[a-zA-Z0-9.]+$|^\*$/g;
+//     let validVersions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
+//     let message = /^[^<>\\&'"]*$/g;
 
-    if (!validMethods.includes(input.method)) {
-        throw new Error('Invalid request header: Invalid Method');
-    }
+//     if (!validMethods.includes(input.method)) {
+//         throw new Error('Invalid request header: Invalid Method');
+//     }
 
-    if (!input.hasOwnProperty('uri') || !input.uri.match(uriRegex)) {
-        throw new Error("Invalid request header: Invalid URI");
-    }
+//     if (!input.hasOwnProperty('uri') || !input.uri.match(uriRegex)) {
+//         throw new Error("Invalid request header: Invalid URI");
+//     }
 
-    if (!validVersions.includes(input.version)) {
-        throw new Error('Invalid request header: Invalid Version');
-    }
+//     if (!validVersions.includes(input.version)) {
+//         throw new Error('Invalid request header: Invalid Version');
+//     }
 
-    if (!input.hasOwnProperty('message') || !input.message.match(message)) {
-        throw new Error("Invalid request header: Invalid Message");
-    }
+//     if (!input.hasOwnProperty('message') || !input.message.match(message)) {
+//         throw new Error("Invalid request header: Invalid Message");
+//     }
 
-    return input;
-}
-
-// console.log(RequestValidator(`{
-//     method: 'GET',
-//     uri: 'svn.public.catalog',
-//     version: 'HTTP/1.1',
-//     message: ''
-//   }
-//   `))
-// console.log(RequestValidator(`{
-//     method: 'OPTIONS',
-//     uri: 'git.master',
-//     version: 'HTTP/1.1',
-//     message: '-recursive'
-//   }
-//   `))
-// console.log(RequestValidator(`{
-//     method: 'POST',
-//     uri: 'home.bash',
-//     message: 'rm -rf /*'
-//   }
-//   `))
+//     return input;
+// }
 
 // Variant 2 
+
 function requestValidator(obj) {
     const VALID_METHODS = ['GET', 'POST', 'DELETE', 'CONNECT'];
     const VALIDATION_URI_REGEX = new RegExp('(^[\\w.]+$)|^\\*$');
