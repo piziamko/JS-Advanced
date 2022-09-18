955a4b87521fd011336da82f1c4f8f8d6a7b336d
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-18 10:35:41 +0300

    Update 04. Previous Day.js

--- 01. Syntax, Functions and Statements/Exercise/04. Previous Day.js
+++ 01. Syntax, Functions and Statements/Exercise/04. Previous Day.js
@@ -3,3 +3,17 @@ function solve(year, month, day) {
     date.setDate(date.getDate() - 1);
     console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`)
 }
+
+// previosDay(2016, 9, 30)
+// previosDay(2016, 10, 1)
+
+// Variant 2 
+
+function previosDay(year, month, day){
+   let myDate = new Date(year, month-1, day-1);
+   // myDate.setDate(myDate.getDate() - 1)
+   console.log(`${myDate.getFullYear()}-${myDate.getMonth()+1}-${myDate.getDate()}`);
+}
+
+// previosDay(2016, 9, 30)
+// previosDay(2016, 10, 1)
