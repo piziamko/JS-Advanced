29b6b79ddd20cda665dd58f83fb6ebc3478bade4
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-18 17:08:30 +0300

    Update 05. Time to Walk.js

--- 01. Syntax, Functions and Statements/Exercise/05. Time to Walk.js
+++ 01. Syntax, Functions and Statements/Exercise/05. Time to Walk.js
@@ -1,3 +1,18 @@
+function timeToWalk(steps, footprint, speed) {
+  let distanceMeters = steps * footprint;
+  let speedMetersSec = speed / 3.6;
+  let time = distanceMeters / speedMetersSec;
+  let rest = Math.floor(distanceMeters / 500);
+  let timeMin = Math.floor(time / 60);
+  let timeSec = Math.round(time - timeMin * 60);
+  let timeHr = Math.floor(time / 3600);
+
+  console.log((timeHr < 10 ? '0' : '') + timeHr + ':' + (timeMin + rest < 10 ? '0' : '') +
+    (timeMin + rest) + ':' + (timeSec < 10 ? '0' : '') + timeSec);
+}
+
+// Variant 2
+
 function TimeToWalk(arg1, arg2, arg3) {
     let stepsNumber = Number(arg1);
     let stepsMetersHr = Number(arg2);
