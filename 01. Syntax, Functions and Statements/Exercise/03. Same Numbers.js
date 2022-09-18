54fa3ec14fcbe123b3f41e776a196ce76c6db008
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-18 08:33:51 +0300

    Update 03. Same Numbers.js

--- 01. Syntax, Functions and Statements/Exercise/03. Same Numbers.js
+++ 01. Syntax, Functions and Statements/Exercise/03. Same Numbers.js
@@ -6,4 +6,23 @@ function solve(input){
     console.log(result);
 }
 
-//solve(1234)
+// solve(2222222);
+// solve(1234)
+
+// Variant 2 
+
+// function sameNumbers(num){
+//     let numAsString = num.toString();
+//     let firstDigit = numAsString[0];
+//     let isSame = true;
+//     let sum = 0;
+//     for (let i = 0; i < numAsString.length; i++){
+//         if (numAsString[i] !== firstDigit){
+//             isSame = false;
+//         }
+//         sum += Number(numAsString[i]);
+//     }
+
+//     console.log(isSame);
+//     console.log(sum);
+// }
