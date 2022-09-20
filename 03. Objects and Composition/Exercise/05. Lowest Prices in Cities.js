b9c8e6fe88b9b8ccacaa5a81300d52661ad712a3
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-20 13:45:12 +0300

    Update 05. Lowest Prices in Cities.js

--- 03. Objects and Composition/Exercise/05. Lowest Prices in Cities.js
+++ 03. Objects and Composition/Exercise/05. Lowest Prices in Cities.js
@@ -12,14 +12,14 @@ function solve(array) {
     }
 }
 
-// console.log(
-//     solve([
-//         "Sample Town | Sample Product | 1000",
-//         "Sample Town | Orange | 2",
-//         "Sample Town | Peach | 1",
-//         "Sofia | Orange | 3",
-//         "Sofia | Peach | 2",
-//         "New York | Sample Product | 1000.1",
-//         "New York | Burger | 10",
-//     ])
-// )
+console.log(
+    solve([
+        "Sample Town | Sample Product | 1000",
+        "Sample Town | Orange | 2",
+        "Sample Town | Peach | 1",
+        "Sofia | Orange | 3",
+        "Sofia | Peach | 2",
+        "New York | Sample Product | 1000.1",
+        "New York | Burger | 10",
+    ])
+)
