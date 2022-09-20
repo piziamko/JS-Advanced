1ba8321fb309f3c7ab649a691794deb153577380
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-20 08:17:46 +0300

    Update 06. From JSON to HTML Table.js

--- 03. Objects and Composition/Lab/06. From JSON to HTML Table.js
+++ 03. Objects and Composition/Lab/06. From JSON to HTML Table.js
@@ -1,3 +1,45 @@
+function solve(json) {
+    let parsed = JSON.parse(json);
+    
+    let columnNames = Object.keys(parsed[0]);
+    let values = parsed.map(obj => Object.values(obj));
+    
+    let result = '<table>\n';
+    
+    appendHeaders(columnNames);
+    appendValues(values);
+    
+    result += '</table>';
+    
+    function appendHeaders(columnNames) {
+        result += `    <tr>`
+        
+        for (let columnName of columnNames) {
+            result += `<th>${columnName}</th>`     
+        }
+        result += `</tr>\n`
+    }
+    
+    function appendValues(values) {
+        
+        for (let value of values) {
+            result += `    <tr>`
+            result += `<td>${escape(value[0])}</td><td>${escape(value[1])}</td>`
+            result += `</tr>\n`
+        }
+    }
+    
+    function escape(value) {
+        return value.toString().replace('<', '&lt;').replace('>', '&gt;');
+    }
+    
+    console.log(result);
+}
+
+fromJSONToHTMLTable('[{"Name":"Stamat","Score":5.5},{"Name":"Rumen","Score":6}]');
+fromJSONToHTMLTable('[{"Name":"Pesho","Score":4," Grade":8},{"Name":"Gosho","Score":5," Grade":8},{"Name":"Angel","Score":5.50," Grade":10}]');
+
+// Variant 2 
 function fromJSONToHTMLTable(json) {
     let arr = JSON.parse(json);
 
