477ad0f8de7da3d0ec11cdf4875d300e735b9fcf
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-20 06:31:15 +0300

    Update 06. From JSON to HTML Table.js

--- 03. Objects and Composition/Lab/06. From JSON to HTML Table.js
+++ 03. Objects and Composition/Lab/06. From JSON to HTML Table.js
@@ -37,5 +37,5 @@ function fromJSONToHTMLTable(json) {
     }
 }
 
-// fromJSONToHTMLTable('[{"Name":"Stamat","Score":5.5},{"Name":"Rumen","Score":6}]');
-// fromJSONToHTMLTable('[{"Name":"Pesho","Score":4," Grade":8},{"Name":"Gosho","Score":5," Grade":8},{"Name":"Angel","Score":5.50," Grade":10}]');
+fromJSONToHTMLTable('[{"Name":"Stamat","Score":5.5},{"Name":"Rumen","Score":6}]');
+fromJSONToHTMLTable('[{"Name":"Pesho","Score":4," Grade":8},{"Name":"Gosho","Score":5," Grade":8},{"Name":"Angel","Score":5.50," Grade":10}]');
