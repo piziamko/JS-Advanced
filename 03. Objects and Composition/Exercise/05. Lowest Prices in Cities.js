9e2af4a703a10af5195015a2d1e6f7330720c2fa
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-21 17:26:44 +0300

    Update 05. Lowest Prices in Cities.js

--- 03. Objects and Composition/Exercise/05. Lowest Prices in Cities.js
+++ 03. Objects and Composition/Exercise/05. Lowest Prices in Cities.js
@@ -1,3 +1,41 @@
+function lowestInPriceCities(data) {
+    let res = {};
+
+    for (let el of data) {
+        let [town, product, price] = el.split(" | ");
+        price = Number(price);
+
+        if (res.hasOwnProperty(product)) {
+            let currentPrice = res[product]["price"];
+            if (currentPrice > price) {
+                res[product] = { town, price }
+            }
+        } else {
+            res[product] = { town, price }
+        }
+
+    }
+
+        for (let [product, value] of Object.entries(res)) {
+            console.log(`${product} -> ${value.price} (${value.town})`)
+    }
+
+    // console.table(res)
+}
+
+
+lowestInPriceCities([
+    "Sample Town | Sample Product | 1000",
+    "Sample Town | Orange | 2",
+    "Sample Town | Peach | 1",
+    "Sofia | Orange | 3",
+    "Sofia | Peach | 2",
+    "New York | Sample Product | 1000.1",
+    "New York | Burger | 10",
+])
+
+// Variant 2 
+
 function solve(array) {
     let obj = {};
     for (const line of array) {
