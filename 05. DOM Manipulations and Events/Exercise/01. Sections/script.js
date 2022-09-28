aed0844e55f02bbca64240e654cdf195392f9eda
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-09-28 08:43:04 +0000

    Update script.js

--- 05. DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ 05. DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,26 +1,49 @@
-function create(arr) {
-    const elements = []
-    const output = document.getElementById("content")
 
-    function eFactory(tag, content = "") {
-        const temp = document.createElement(tag)
-        temp.innerHTML = content
-
-        return temp
+function create(words) {
+    for (let word of words) {
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        p.innerText = word;
+        p.style.display = "none";
+        div.appendChild(p);
+        div.addEventListener("click", showP);
+        document.getElementById("content").appendChild(div)
     }
-
-    arr.forEach(x => {
-        const div = eFactory("div")
-        const p = eFactory("p", x)
-        p.style.display = "none"
-        
-        div.appendChild(p)
-        div.addEventListener("click", () => (p.style.display = "block"))
-        elements.push(div)
-    })
-
-    elements.forEach(x => output.appendChild(x))
+    
+    function showP(event) {
+        if (event.target.nodeName === "P") {
+            return
+        }
+        let p = event.target.children[0];
+        p.style.display = "block"
+    }
 }
+
+// Variant 1
+
+// function create(arr) {
+//     const elements = []
+//     const output = document.getElementById("content")
+
+//     function eFactory(tag, content = "") {
+//         const temp = document.createElement(tag)
+//         temp.innerHTML = content
+
+//         return temp
+//     }
+
+//     arr.forEach(x => {
+//         const div = eFactory("div")
+//         const p = eFactory("p", x)
+//         p.style.display = "none"
+        
+//         div.appendChild(p)
+//         div.addEventListener("click", () => (p.style.display = "block"))
+//         elements.push(div)
+//     })
+
+//     elements.forEach(x => output.appendChild(x))
+// }
 
 // Variant 2
 
