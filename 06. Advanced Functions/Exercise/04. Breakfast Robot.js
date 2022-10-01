a9b807afb23a776cebde2dd327f8d768d1156b22
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 07:03:59 +0000

    Update 04. Breakfast Robot.js

--- 06. Advanced Functions/Exercise/04. Breakfast Robot.js
+++ 06. Advanced Functions/Exercise/04. Breakfast Robot.js
@@ -1,65 +1,75 @@
-function closure() {
-    const ingreds = {
-        protein: 0,
-        carbohydrate: 0,
-        fat: 0,
-        flavour: 0,
-    }
-    const recipes = {
-        apple: parseRecipeData(0, 1, 0, 2),
-        lemonade: parseRecipeData(0, 10, 0, 20),
-        burger: parseRecipeData(0, 5, 7, 3),
-        eggs: parseRecipeData(5, 0, 1, 1),
-        turkey: parseRecipeData(10, 10, 10, 10),
-    }
+function solution() {
+  let microelements = {
+    protein: 0,
+    carbohydrate: 0,
+    fat: 0,
+    flavour: 0,
+  };
 
-    function parseRecipeData(protein, carbohydrate, fat, flavour) {
-        return {
-            protein,
-            carbohydrate,
-            fat,
-            flavour,
+  let recipes = {
+    apple: { carbohydrate: 1, flavour: 2 },
+    lemonade: { carbohydrate: 10, flavour: 20 },
+    burger: { carbohydrate: 5, fat: 7, flavour: 3 },
+    eggs: { protein: 5, fat: 1, flavour: 1 },
+    turkey: { protein: 10, carbohydrate: 10, fat: 10, flavour: 10 },
+  };
+  let output = "";
+
+  let obj = {
+    restock: (microelementOrRecipe, quantity) => {
+      microelements[microelementOrRecipe] += quantity;
+      output = "Success";
+    },
+    prepare: (microelementOrRecipe, quantity) => {
+      let entries = Object.entries(recipes[microelementOrRecipe]);
+      for (let entry of entries) {
+        let [key, value] = entry;
+        if (microelements[key] < value * quantity) {
+          return (output = `Error: not enough ${key} in stock`);
         }
-    }
-
-    function restock(ingr, x) {
-        ingreds[ingr] += x
-        return "Success"
-    }
-
-    function prepare(required, quantity) {
-        const parsedRecipe = Object.entries(required).map(x => [
-            x[0],
-            x[1] * quantity,
-        ])
-
-        for (let i = 0; i < parsedRecipe.length; i++) {
-            const [name, amount] = parsedRecipe[i]
-            if (ingreds[name] - amount < 0) {
-                return `Error: not enough ${name} in stock`
-            }
+      }
+      for (let entry of entries) {
+        let [key, value] = entry;
+        if ((microelements[key] -= value * quantity)) {
+          output = "Success";
         }
+      }
+    },
+    report: () => {
+      reportOutput = [];
+      Object.entries(microelements).forEach((x) => {
+        reportOutput.push(`${x[0]}=${x[1]}`);
+      });
+      output = reportOutput.join(" ");
+    },
+  };
 
-        parsedRecipe.forEach(([name, amount]) => (ingreds[name] -= amount))
-        return "Success"
-    }
+  return function manager(instructions) {
+    let [command, microelementOrRecipe, quantity] = instructions.split(" ");
+    quantity = Number(quantity);
+    obj[command](microelementOrRecipe, quantity);
 
-    const report = () =>
-        Object.entries(ingreds)
-            .map(([key, value]) => `${key}=${value}`)
-            .join(" ")
-
-    const actions = {
-        prepare: (r, q) => prepare(recipes[r], q),
-        restock,
-        report,
-    }
-
-    return s => {
-        const [command, a, b] = s
-            .split(" ")
-            .map(x => (isNaN(x) ? x : Number(x)))
-
-        return actions[command](a, b)
-    }
+    return output;
+  };
 }
+
+let manager = solution();
+// console.log(manager("restock flavour 50")); // Success
+// console.log(manager("prepare lemonade 4")); // Error: not enough carbohydrate in stock
+// console.log(manager("restock carbohydrate 10"));
+// console.log(manager("restock flavour 10"));
+// console.log(manager("prepare apple 1"));
+// console.log(manager("restock fat 10"));
+// console.log(manager("prepare burger 1"));
+// console.log(manager("report"));
+
+console.log(manager("prepare turkey 1"));
+console.log(manager("restock protein 10"));
+console.log(manager("prepare turkey 1"));
+console.log(manager("restock carbohydrate 10"));
+console.log(manager("prepare turkey 1"));
+console.log(manager("restock fat 10"));
+console.log(manager("prepare turkey 1"));
+console.log(manager("restock flavour 10"));
+console.log(manager("prepare turkey 1"));
+console.log(manager("report"));
