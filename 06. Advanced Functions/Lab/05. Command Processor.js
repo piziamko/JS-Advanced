0f3e5097431aa8b22a9240bc7d57a851ad272501
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 06:09:57 +0000

    Update 05. Command Processor.js

--- 06. Advanced Functions/Lab/05. Command Processor.js
+++ 06. Advanced Functions/Lab/05. Command Processor.js
@@ -1,10 +1,36 @@
-function solve() {
-    let str = ""
-
+function solution() {
+    let result = '';
+    
     return {
-        append: v => (str += v),
-        removeStart: x => (str = str.slice(x)),
-        removeEnd: x => (str = str.slice(0, -x)),
-        print: () => console.log(str),
+        append(string) {
+            result += string;
+        },
+        removeStart(n) {
+            result = result.slice(n);
+        },
+        removeEnd(n) {
+            result = result.slice(0, result.length - n);
+        },
+        print() {
+            console.log(result);
+        }
     }
 }
+
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+
+let secondZeroTest = solution();
+
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
