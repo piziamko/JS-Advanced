e00d45bd2f759e0c47f68fb430b8a3ed91112bf3
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 06:03:51 +0000

    Update 03. Currency Format.js

--- 06. Advanced Functions/Lab/03. Currency Format.js
+++ 06. Advanced Functions/Lab/03. Currency Format.js
@@ -1,3 +1,8 @@
 function solve(a, b, c, d) {
     return d.bind(undefined, a, b, c)
 }
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
