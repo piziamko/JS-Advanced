a1573aa81ef80454a6de9be5a7c98fd018a852a7
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 07:07:48 +0000

    Update 06.  Monkey Patcher.js

--- 06. Advanced Functions/Exercise/06.  Monkey Patcher.js
+++ 06. Advanced Functions/Exercise/06.  Monkey Patcher.js
@@ -1,36 +1,59 @@
-function result (command) {
-    const actions = {
-        upvote: x => (x.upvotes += 1), downvote: x => (x.downvotes += 1), score: x => getScore(x),
+function solution(command) {
+
+    if (command === 'upvote') {
+        return this.upvotes++;
+    } else if (command === 'downvote') {
+        return this.downvotes++;
     }
 
-    function isObfuscable (votes) {
-        return votes > 50
+    let upvotes = this.upvotes;
+    let downvotes = this.downvotes;
+    let totalVotes = upvotes + downvotes;
+    let balance = upvotes - downvotes;
+
+    function ratingState() {
+        if (totalVotes < 10) {
+            return 'new';
+        } else if (upvotes > totalVotes * 0.66) {
+            return 'hot';
+        } else if (balance >= 0 && totalVotes > 100) {
+            return 'controversial'
+        } else if (balance < 0) {
+            return 'unpopular';
+        } else {
+            return 'new';
+        }
     }
 
-    function getObfNum (u, d) {
-        return Math.ceil(0.25 * Math.max(u, d))
+    if (totalVotes > 50) {
+        let obfuscate = Math.ceil(Math.max(upvotes, downvotes) * 0.25);
+        return [upvotes + obfuscate, downvotes + obfuscate, balance, ratingState()];
     }
 
-    function calcRating (u, d) {
-        //Branch for me, branch for you, branch for everyonnee... :-)
+    return [upvotes, downvotes, balance, ratingState.call(this)];
 
-        if (u + d < 10) return 'new'
-        if (u > (u + d) * 0.66) return 'hot'
-        if (u - d >= 0 && (u > 100 || d > 100)) return 'controversial'
-        if (u - d < 0) return 'unpopular'
-
-        return 'new'
-    }
-
-    function getScore ({upvotes, downvotes}) {
-        const obfNum = isObfuscable(upvotes + downvotes) ? getObfNum(upvotes, downvotes) : 0
-        const [rUps, rDowns] = [upvotes + obfNum, downvotes + obfNum]
-        const balance = rUps - rDowns
-
-        return [rUps, rDowns, balance, calcRating(upvotes, downvotes)]
-    }
-
-    return actions[command](this)
 }
 
-//result.call({upvotes: 1, downvotes: 2}, 'upvote')
+
+let post = {
+    id: '3',
+    author: 'emil',
+    content: 'wazaaaaa',
+    upvotes: 100,
+    downvotes: 100
+};
+// let post = {
+//     id: '3',
+//     author: 'emil',
+//     content: 'wazaaaaa',
+//     upvotes: 0,
+//     downvotes: 0
+// };
+solution.call(post, 'upvote');
+solution.call(post, 'downvote');
+let score = solution.call(post, 'score');
+console.log(score); // [127, 127, 0, 'controversial']
+solution.call(post, 'downvote');         // (executed 50 times)
+
+score = solution.call(post, 'score');
+console.log(score);    // [139, 189, -50, 'unpopular']
