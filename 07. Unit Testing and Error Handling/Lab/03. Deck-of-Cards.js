a1e76dec3aec7036d27680ca81dfb6f9b122ecac
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 10:58:12 +0000

    Update 03. Deck-of-Cards.js

--- 07. Unit Testing and Error Handling/Lab/03. Deck-of-Cards.js
+++ 07. Unit Testing and Error Handling/Lab/03. Deck-of-Cards.js
@@ -1,49 +1,44 @@
-function createDeck(cards) {
+function printDeckOfCards(cards) {
     let result = [];
-    for (let card of cards) {
-        const face = card.slice(0, -1);
-        const suit = card.slice(-1);
-        try {
-            result.push(createCard(face, suit));
-        } catch (err) {
-            console.log('Invalid card: ' + card);
-            return;
-        }
 
+    try {
+        cards.forEach(card => {
+            let cardSuit = card.charAt(card.length - 1);
+            let cardFace = card.slice(0, card.length - 1)
+            result.push(createCard(cardFace, cardSuit).toString());
+        });
+
+        console.log(result.join(' '));
+
+    } catch (error) {
+        console.log(error.message);
     }
-    console.log(result.join(' '));
-    /* console.log(cards.map(card => {
-         const face = card.slice(0, -1);
-         const suit = card.slice(-1);
-         try {
-         return createCard(face, suit);
-         } catch (err) {
-             
-         }
-     }).join(' ')); */
-    function createCard(face, suit) {
-        const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-        const suits = {
+
+    function createCard(cardFace, cardSuit) {
+        const validCardFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const validCardSuits = {
             'S': '\u2660',
             'H': '\u2665',
             'D': '\u2666',
             'C': '\u2663',
-        };
-        if (faces.includes(face) == false) {
-            throw new Error('Invalid face: ' + face)
         }
 
-        if (Object.keys(suits).includes(suit) == false) {
-            throw new Error('Invalid suit ' + suit);
+        if (!validCardFaces.includes(cardFace) || !Object.keys(validCardSuits).includes(cardSuit)) {
+            throw new Error(`Invalid card: ${cardFace}${cardSuit}`);
         }
 
         return {
-            face,
-            suit: suits[suit],
+            cardFace,
+            cardSuit,
             toString() {
-                return this.face + this.suit;
+                return this.cardFace + validCardSuits[this.cardSuit];
             }
         }
     }
-
 }
+
+cards = ['AS', '10D', 'KH', '2C'];
+printDeckOfCards(cards);
+
+cards2 = ['5S', '3D', 'QD', '1C'];
+printDeckOfCards(cards2);
