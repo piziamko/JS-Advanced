ff4686af2766139d120a8fd6324152a9560440ff
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 07:02:59 +0000

    Update 03. Fibonacci.js

--- 06. Advanced Functions/Exercise/03. Fibonacci.js
+++ 06. Advanced Functions/Exercise/03. Fibonacci.js
@@ -1,34 +1,21 @@
-// function getFibonator() {
-//     let [prev, curr] = [1, 0];
+function getFibonator() {
+    let num1 = 0;
+    let num2 = 1;
+    let result = 0;
 
-//     return () => {
-//         let next = prev + curr;
-//         prev = curr;
-//         curr = next;
-//         return curr;
-//     }
-// }
-
-//let fib = getFibonator();
-//console.log(fib());
-//console.log(fib());
-//console.log(fib());
-//console.log(fib());
-//console.log(fib());
-//console.log(fib());
-//console.log(fib());
-
-// Variant 2
-
-function solve() {
-    const lastNumbers = [0, 0];
-
-    function fib() {
-        const result = lastNumbers.reduce((acc, num) => acc + num, 0) || 1;
-        lastNumbers.shift()
-        lastNumbers.push(result)
-        return result
+    return function fib() {
+        result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+        return num1;
     }
-
-    return fib;
 }
+
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
