99737ea87e3b47f05a2b09e46e7fdc121e1f1d6f
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-01 06:03:09 +0000

    Update 02. Add.js

--- 06. Advanced Functions/Lab/02. Add.js
+++ 06. Advanced Functions/Lab/02. Add.js
@@ -1,7 +1,15 @@
-function solve(n) {
-    let num = n
-
-    return function add(n) {
-        return num + n
+function solution(num) {
+    function add (a, b) {
+        return a + b;
     }
+    return add.bind(this, num)
 }
+
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
