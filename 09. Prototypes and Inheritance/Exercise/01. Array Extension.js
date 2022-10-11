d99842c4f0709ca48ed0310df67038a681974829
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-11 17:06:47 +0000

    Update 01. Array Extension.js

--- 09. Prototypes and Inheritance/Exercise/01. Array Extension.js
+++ 09. Prototypes and Inheritance/Exercise/01. Array Extension.js
@@ -1,21 +1,30 @@
-(function solve() {
-    Array.prototype.last = function () {
+function arrayExtension () {
+    let myArr = [1, 2, 3, 4];
+    let anotherArr = [9, 8, 7, 6, 5];
+    
+    Array.prototype.last = function() {
         return this[this.length - 1];
     }
-
-    Array.prototype.sum = function () {
-        return this.reduce((acc, x) => acc + x, 0);
-    }
-
-    Array.prototype.average = function () {
-        return this.sum() / this.length;
-    }
-
-    Array.prototype.skip = function (n) {
+    
+    Array.prototype.skip = function(n) {
         return this.slice(n);
     }
-
-    Array.prototype.take = function (n) {
+    
+    Array.prototype.take = function(n) {
         return this.slice(0, n);
     }
-}());
+    
+    Array.prototype.sum = function() {
+        return this.reduce((prevValue, nextValue) => prevValue + nextValue);
+    }
+    
+    Array.prototype.average = function(){
+        return this.sum() / this.length;
+    }
+    
+    console.table(Array.prototype);
+    console.log(myArr.sum());
+    console.log(anotherArr.sum());
+}
+
+arrayExtension()
