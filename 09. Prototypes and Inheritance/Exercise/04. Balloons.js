ecad20ca344ef4a5cbf50cad9bce85c29565feb8
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-11 12:08:52 +0000

    Update 04. Balloons.js

--- 09. Prototypes and Inheritance/Exercise/04. Balloons.js
+++ 09. Prototypes and Inheritance/Exercise/04. Balloons.js
@@ -1,44 +1,41 @@
 function solution() {
-
-    class Balloon {
-        constructor(color, hasWeight) {
-            this.color = color;
-            this.hasWeight = hasWeight;
-        }
+  class Balloon {
+    constructor(color, hasWeight) {
+      this.color = color;
+      this.hasWeight = Number(hasWeight);
+    }
+  }
+  class PartyBalloon extends Balloon {
+    constructor(color, hasWeight, ribbonColor, ribbonLength) {
+      super(color, hasWeight);
+      this._ribbon = {
+        color: ribbonColor,
+        length: Number(ribbonLength),
+      };
     }
 
-    class PartyBalloon extends Balloon {
-        constructor(color, hasWeight, ribbonColor, ribbonLength) {
-            super(color, hasWeight);
-            this._ribbon = { color: ribbonColor, length: ribbonLength };
-        }
-
-        get ribbon() {
-            return this._ribbon;
-        }
-
+    get ribbon() {
+      return this._ribbon;
+    }
+  }
+  class BirthdayBalloon extends PartyBalloon {
+    constructor(color, hasWeight, ribbonColor, ribbonLength, text) {
+      super(color, hasWeight, ribbonColor, ribbonLength);
+      this._text = text;
     }
 
-    class BirthdayBalloon extends PartyBalloon {
-        constructor(color, hasWeight, ribbonColor, ribbonLength, text) {
-            super(color, hasWeight, ribbonColor, ribbonLength);
-            this._text = text;
-        }
-
-        get text() {
-            return this._text;
-        }
+    get text() {
+      return this._text;
     }
+  }
 
-    return { Balloon, PartyBalloon, BirthdayBalloon };
+  return { Balloon, PartyBalloon, BirthdayBalloon };
 }
 
-
-// let a = solve()
-// let class1 = new a.Balloon('red', 45)
-// console.log(class1);
-// let class2 = new a.PartyBalloon('red', 45, 'blue', 1000)
-// console.log(class2);
-// let class3 = new a.BirthdayBalloon('red', 45, 'purple', 10, 'text')
-
-// console.log(class3.ribbon);
+let classes = solution();
+let testBalloon = new classes.Balloon("yellow", 20.5);
+let testPartyBalloon = new classes.PartyBalloon("yellow", 20.5, "red", 10.25);
+let ribbon = testPartyBalloon.ribbon;
+console.log(testBalloon);
+console.log(testPartyBalloon);
+console.log(ribbon);
