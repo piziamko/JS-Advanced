c88c4b56e5f33cf2fafdf7ec2e9b190df357d4aa
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-11 12:08:11 +0000

    Update 03. Extensible Object.js

--- 09. Prototypes and Inheritance/Exercise/03. Extensible Object.js
+++ 09. Prototypes and Inheritance/Exercise/03. Extensible Object.js
@@ -1,16 +1,23 @@
-function solve() {
-    const proto = {};
-    const inst = Object.create(proto);
+function extensibleObject() {
+  let objProto = Object.getPrototypeOf(this);
+  //Get the prototype of the object and add inside it only if the value is a function
 
-    inst.extend = function (templates) {
-        Object.entries(templates).forEach(([key, value]) => {
-            if (typeof value === 'function') {
-                proto[key] = value;
-            } else {
-                inst[key] = value;
-            }
-        })
-    };
-
-    return inst;
+  this.extend = function (obj) {
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value == "function") {
+        objProto[key] = value;
+      } else {
+        this[key] = value;
+      }
+    }
+  };
+  return this;
 }
+
+const template = {
+  extensionMethod: function () {},
+  extensionProperty: "someString",
+};
+
+const myObj = extensibleObject();
+myObj.extend(template);
