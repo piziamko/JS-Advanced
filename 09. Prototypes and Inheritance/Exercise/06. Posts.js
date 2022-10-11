16629f80a83df09043299b1c392bc5debe4361fc
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-11 12:10:16 +0000

    Update 06. Posts.js

--- 09. Prototypes and Inheritance/Exercise/06. Posts.js
+++ 09. Prototypes and Inheritance/Exercise/06. Posts.js
@@ -56,3 +56,27 @@ function solution() {
 
     return { Post, SocialMediaPost, BlogPost };
 }
+
+const classes = solution();
+let post = new classes.Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!
