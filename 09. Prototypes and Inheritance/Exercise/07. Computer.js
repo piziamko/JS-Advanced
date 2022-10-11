4c27d305577d5ab974edb31d47629503f800f505
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-10-11 12:11:03 +0000

    Update 07. Computer.js

--- 09. Prototypes and Inheritance/Exercise/07. Computer.js
+++ 09. Prototypes and Inheritance/Exercise/07. Computer.js
@@ -111,3 +111,16 @@ function createComputerHierarchy() {
         Desktop
     }
 }
+
+let classes = createComputerHierarchy();
+let Computer = classes.Computer;
+let Laptop = classes.Laptop;
+let Desktop = classes.Desktop;
+let Monitor = classes.Monitor;
+let Battery = classes.Battery;
+let Keyboard = classes.Keyboard;
+
+let battery = new Battery("Energy", 3);
+console.log(battery);
+let laptop = new Laptop("Hewlett Packard", 2.4, 4, 0.5, 3.12, "Silver", battery);
+console.log(laptop);
